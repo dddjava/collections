ab8290ee4485573c0679b5e73c697d60980c25b8
masuda <masuda220@gmail.com>
2017-02-22 11:49:48 +0900

    最初のドラフト Groupクラス　安全なjava.util.Set

---
+++ src/test/java/org/dddjava/collections/GroupTest.java
@@ -0,0 +1,54 @@
+package org.dddjava.collections;
+
+public class GroupTest {
+	@org.junit.Test
+	public void size() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void union() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void minus() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void intersect() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void select() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void reject() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void detect() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void detectOrDefault() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void asSorted() throws Exception {
+
+	}
+
+	@org.junit.Test
+	public void asSorted1() throws Exception {
+
+	}
+
+}
